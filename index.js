115adb6455781480586d40125bd36a9667d30c17
Harsh Mehta <113175089+HarshMehta2522@users.noreply.github.com>
2023-08-04 00:33:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const express = require("express");
-const stripe = require("stripe")(
-  "sk_test_51NZXGVSGyUMmynkIQK6bBxHxhTWQhvSSJkAeyutKvOqEOgAVgttQ5jdP22dUorUJR2Hqlz7anAQulWq06OQHNh3M00hy6lPhDQ"
-);
-const app = express();
-const cors = require("cors");
 
-app.use(express.json());
-app.use(cors({ origin: true }));
-const router=require('./routes/router.js')
-app.use('/api',router)
-
-app.listen(5000, () => {
-  console.log("Running on port 5000");
-});
