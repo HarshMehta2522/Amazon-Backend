6d247044fcf43bc3ff48aae0d786d6007cee4e0f
Harsh Mehta <113175089+HarshMehta2522@users.noreply.github.com>
2023-08-04 17:45:39 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,9 @@ const router = express.Router();
 router.get("/health-check", (req, res) => {
   res.send("Working").status(200);
 });
+const stripe = require("stripe")(
+    "sk_test_51NZXGVSGyUMmynkIQK6bBxHxhTWQhvSSJkAeyutKvOqEOgAVgttQ5jdP22dUorUJR2Hqlz7anAQulWq06OQHNh3M00hy6lPhDQ"
+  );
 
 router.post("/payment/create", async (req, response) => {
   console.log(req.body);
