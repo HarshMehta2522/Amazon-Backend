27f03d46b9f6198befe20f1b1d0f7e7b4b2fae6c
Harsh Mehta <113175089+HarshMehta2522@users.noreply.github.com>
2023-08-04 17:38:31 +0530

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -3,8 +3,14 @@ const serverless = require("serverless-http");
 const cors = require("cors");
 const app = express();
 const authRouter = require("../routes/router.js");
-app.use(cors({"origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"}));
+// app.use(cors({"origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin (*)
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json());
 
 
